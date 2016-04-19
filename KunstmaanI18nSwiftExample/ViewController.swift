19ed65d302ea1c58c08e251ea6b8dcb750fd4398
Daan Poron <daan@poron.be>
2016-04-19 13:57:46 +0200

    Initial Commit

---
+++ KunstmaanI18nSwiftExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  KunstmaanI18nSwiftExample
+//
+//  Created by Daan Poron on 19/04/16.
+//  Copyright © 2016 Kunstmaan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
